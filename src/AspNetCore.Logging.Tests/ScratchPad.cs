22c303f30366183133b9bfb957dc1e2ad6d6c8e9
oakio <ilya.dubadenko@gmail.com>
2017-11-08 12:50:28 +0300

    Big Bang!

---
+++ src/AspNetCore.Logging.Tests/ScratchPad.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Codestellation.AspNetCore.Logging.Tests
+{
+    [TestFixture]
+    public class ScratchPad
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
