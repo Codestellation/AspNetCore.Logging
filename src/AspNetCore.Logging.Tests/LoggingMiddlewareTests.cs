760e22218ab4d5f339a7365d3e585036f8906d06
oakio <ilya.dubadenko@gmail.com>
2019-10-18 13:30:17 +0300

    Using HttpContextLogEventFormatter and PooledMemoryStream

--- src/AspNetCore.Logging.Tests/LoggingMiddlewareTests.cs
+++ src/AspNetCore.Logging.Tests/LoggingMiddlewareTests.cs
@@ -1,11 +1,13 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Logging.Console;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 
 namespace Codestellation.AspNetCore.Logging.Tests
@@ -18,24 +20,27 @@ namespace Codestellation.AspNetCore.Logging.Tests
         public async Task Smoke()
         {
             // given
-            var logger = new ConsoleLogger(nameof(LoggingMiddlewareTests), null, true);
-
+            ILoggerFactory loggerFactory = LoggerFactory.Create(b => b.AddConsole());
+            ILogger logger = loggerFactory.CreateLogger<LoggingMiddlewareTests>();
             IWebHostBuilder builder = new WebHostBuilder()
                 .Configure(
                     app =>
                     {
                         app.UseMiddleware<LoggingMiddleware>(logger);
-                        app.Run(async context => { await context.Response.WriteAsync("Hello World!").ConfigureAwait(false); });
+                        app.Run(async context =>
+                        {
+                            await context.Request.Body.DrainAsync(CancellationToken.None).ConfigureAwait(false);
+                            await context.Response.WriteAsync("Hello World!").ConfigureAwait(false);
+                        });
                     });
 
             var server = new TestServer(builder);
             HttpClient client = server.CreateClient();
             client.DefaultRequestHeaders.Add("X-MyHeader", "myValue");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", "dXNlcjpwYXNzd29yZAo=");
-            
 
             // when
-            HttpResponseMessage response = await client.GetAsync("/path?query=abc").ConfigureAwait(false);
+            HttpResponseMessage response = await client.PostAsync("/path?query=abc", new StringContent("{\"text\": \"Hello!\"}")).ConfigureAwait(false);
 
             // than
             response.EnsureSuccessStatusCode();
--- src/AspNetCore.Logging/LoggingMiddleware.cs
+++ src/AspNetCore.Logging/LoggingMiddleware.cs
@@ -1,38 +1,27 @@
 using System;
-using System.Collections.Generic;
+using System.Buffers;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
-using Codestellation.AspNetCore.Logging.Masking;
+using Codestellation.AspNetCore.Logging.Format;
+using Codestellation.AspNetCore.Logging.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.ObjectPool;
-using Microsoft.Extensions.Primitives;
-using Microsoft.Net.Http.Headers;
 
 namespace Codestellation.AspNetCore.Logging
 {
     public class LoggingMiddleware
     {
-        private static readonly Encoding Encoding = Encoding.UTF8;
-
-        private const int PoolSize = 8;
+        private static readonly ArrayPool<byte> BufferPool = ArrayPool<byte>.Shared;
+        private const int ChunkSize = 256;
+        private static readonly HttpContextLogEventFormatter Formatter = new HttpContextLogEventFormatter();
 
         private readonly RequestDelegate _next;
         private readonly ILogger _logger;
-        private readonly ObjectPool<StringBuilder> _stringBuilderPool;
-        private readonly ObjectPool<MemoryStream> _streamPool;
 
         public LoggingMiddleware(RequestDelegate next, ILogger logger)
         {
             _next = next;
             _logger = logger;
-
-            var stringBuilderPolicy = new StringBuilderPooledObjectPolicy(4096);
-            _stringBuilderPool = new DefaultObjectPool<StringBuilder>(stringBuilderPolicy, PoolSize);
-
-            var memoryStreamPolicy = new MemoryStreamPooledObjectPolicy(2024);
-            _streamPool = new DefaultObjectPool<MemoryStream>(memoryStreamPolicy, 2 * PoolSize); // x2 request+response
         }
 
         public async Task Invoke(HttpContext context)
@@ -46,10 +35,8 @@ namespace Codestellation.AspNetCore.Logging
             HttpRequest request = context.Request;
             HttpResponse response = context.Response;
 
-            StringBuilder output = _stringBuilderPool.Get();
-
-            MemoryStream requestBody = _streamPool.Get();
-            MemoryStream responseBody = _streamPool.Get();
+            var requestBody = new PooledMemoryStream(BufferPool, ChunkSize);
+            var responseBody = new PooledMemoryStream(BufferPool, ChunkSize);
 
             Stream originRequestBody = request.Body;
             Stream originResponseBody = response.Body;
@@ -58,114 +45,57 @@ namespace Codestellation.AspNetCore.Logging
             response.Body = Sniff(originResponseBody, responseBody);
 
             DateTime startedAt = DateTime.Now;
+
+            var logEvent = new HttpContextLogEvent
+            {
+                StartedAt = startedAt,
+                RequestBody = requestBody,
+                ResponseBody = responseBody
+            };
+
             try
             {
-                AppendRequestHeaders(request, output, startedAt); // log request's info before Next.Invoke() called, because HttpRequest may be disposed after
+                LogRequest(request, logEvent); // log request's info before Next.Invoke() called, because HttpRequest may be disposed after
 
                 await _next.Invoke(context).ConfigureAwait(false);
             }
             finally
             {
                 DateTime finishedAt = DateTime.Now;
-                TimeSpan elapsed = finishedAt - startedAt;
+                logEvent.Elapsed = finishedAt - startedAt;
 
                 request.Body = originRequestBody;
                 response.Body = originResponseBody;
 
-                AppendBody(requestBody, output);
-                AppendResponseHeaders(response, output, elapsed);
-                AppendBody(responseBody, output);
+                LogResponse(response, logEvent);
 
-                _logger.LogInformation(output.ToString());
+                string message = Formatter.Format(logEvent);
+                _logger.LogInformation(message);
 
-                _streamPool.Return(requestBody);
-                _streamPool.Return(responseBody);
-                _stringBuilderPool.Return(output);
+                requestBody.Dispose();
+                responseBody.Dispose();
             }
         }
 
         private static Stream Sniff(Stream master, Stream sink) => new SnifferStream(master, sink);
 
-        private void AppendRequestHeaders(HttpRequest request, StringBuilder output, DateTime startedAt)
+        private static void LogRequest(HttpRequest request, HttpContextLogEvent logEvent)
         {
-            string method = request.Method;
-            string uri = $"{request.Scheme}://{request.Host}{request.Path}{request.QueryString}";
-            string protocol = request.Protocol;
-
-            ConnectionInfo connection = request.HttpContext.Connection;
-            string remoteAddress = $"{connection.RemoteIpAddress}:{connection.RemotePort}";
-
-            output.AppendLine($"{method} {uri} {protocol} from {remoteAddress} at {startedAt:HH:mm:ss.fff}");
-
-            AppendHeaders(request.Headers, output);
+            logEvent.Method = request.Method;
+            logEvent.Scheme = request.Scheme;
+            logEvent.Host = request.Host;
+            logEvent.Path = request.Path;
+            logEvent.QueryString = request.QueryString;
+            logEvent.Protocol = request.Protocol;
+            logEvent.RemoteIpAddress = request.HttpContext.Connection.RemoteIpAddress;
+            logEvent.RemotePort = request.HttpContext.Connection.RemotePort;
+            logEvent.RequestHeaders = request.Headers;
         }
 
-        private void AppendResponseHeaders(HttpResponse response, StringBuilder output, TimeSpan elapsed)
+        private static void LogResponse(HttpResponse response, HttpContextLogEvent logEvent)
         {
-            output.AppendLine($"{response.StatusCode} in {(long)elapsed.TotalSeconds}.{elapsed.Milliseconds:000} seconds");
-
-            AppendHeaders(response.Headers, output);
-        }
-
-        private void AppendHeaders(IHeaderDictionary headers, StringBuilder output)
-        {
-            foreach (KeyValuePair<string, StringValues> header in headers)
-            {
-                string name = header.Key;
-                string value = header.Value.ToString();
-
-                bool masking = name == HeaderNames.Authorization;
-                if (masking)
-                {
-                    value = Mask(value);
-                }
-
-                output.AppendLine($"{name}: {value}");
-            }
-        }
-
-        private void AppendBody(MemoryStream body, StringBuilder output)
-        {
-            try
-            {
-                string bodyText = GetString(body);
-                output.AppendLine(bodyText);
-            }
-            catch (Exception error)
-            {
-                AppendBodyReadingException(error, output);
-            }
-        }
-
-        private static string GetString(MemoryStream stream)
-        {
-            if (!stream.TryGetBuffer(out ArraySegment<byte> segment))
-            {
-                return string.Empty;
-            }
-
-            byte[] buffer = segment.Array;
-            if (buffer == null || segment.Count == 0)
-            {
-                return string.Empty;
-            }
-
-            return Encoding.GetString(buffer, segment.Offset, segment.Count);
-        }
-
-        private static void AppendBodyReadingException(Exception error, StringBuilder output)
-        {
-            const string disclaimer = "--- this is not the part of the request or response ---";
-            output.AppendLine(disclaimer);
-            output.AppendLine("Failed to read the body");
-            output.AppendLine(error.ToString());
-            output.AppendLine(disclaimer);
-        }
-
-        public static string Mask(string value)
-        {
-            uint mask = FNV1a.Hash(value);
-            return $"<FNV-1a:{mask}>";
+            logEvent.StatusCode = response.StatusCode;
+            logEvent.ResponseHeaders = response.Headers;
         }
     }
 }
